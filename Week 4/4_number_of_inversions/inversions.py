320ba2ce4cb85c9259f1f45cc6f553c93cbe4202
ExpressHermes <9876shivamyadav@gmail.com>
2020-06-05 09:54:14 +0530

    Added Inversions

--- Week 4/4_number_of_inversions/inversions.py
+++ Week 4/4_number_of_inversions/inversions.py
@@ -1,18 +1,41 @@
 # Uses python3
 import sys
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
-    ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+inversion_count = 0
+def merge(p, q):
+    i, j = 0, 0
+    global inversion_count
+    c = []
+    while i < len(p) and j < len(q):
+        if p[i] > q[j]:
+            inversion_count += len(p) - i
+            c.append(q[j])
+            j += 1
+        else:
+            c.append(p[i])
+            i += 1
+    
+    while i < len(p):
+        c.append(p[i])
+        i += 1
+    
+    while j < len(q):
+        c.append(q[j])
+        j += 1
+    
+    return c
+
+def merge_sort(a):
+    if len(a) == 1:
+        return a
+
+    mid = len(a) // 2
+    p = merge_sort(a[:mid])
+    q = merge_sort(a[mid:])
+    return merge(p, q)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+    merge_sort(a)
+    print(inversion_count)
